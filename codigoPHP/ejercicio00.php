767229ac7dcc763b5729435c22d732b98658eaf7
David <David.pralos@educa.jcyl.es>
2021-10-18 09:02:20 +0200

    Version 1 - 18/10/2021

---
+++ codigoPHP/ejercicio00.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Ejercicio 00</title>
+    </head>
+    <body>
+        <?php
+            echo "Hola mundo";
+            phpinfo();
+        ?>
+    </body>
+</html>
---
+++ codigoPHP/ejercicio01.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Ejercicio 01</title>
+    </head>
+    <body>
+        <?php
+            
+        ?>
+    </body>
+</html>
+
